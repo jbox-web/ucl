f13d1d406f3182fafef0739fa82c9e59067244d8
Nicolas Rodriguez <nicoladmin@free.fr>
2020-04-15 04:54:16 +0200

    Add tests on thread safety

--- spec/ucl_spec.rb
+++ spec/ucl_spec.rb
@@ -6,12 +6,34 @@ RSpec.describe UCL do
     it 'can decode UCL data with .load' do
       expect(UCL.load('foo = bar')).to eq({ 'foo' => 'bar' })
     end
+
+    it 'should be threadsafe' do
+      threads = []
+      (1..10).each do |i|
+        threads << Thread.new do
+          expect(UCL.load("foo = #{i}")).to eq({ 'foo' => i })
+        end
+      end
+
+      threads.map(&:join)
+    end
   end
 
 
   describe '.dump' do
     it 'can encode UCL data with .dump' do
       expect(UCL.dump({ 'foo' => 'bar' })).to eq("foo = \"bar\";\n")
+    end
+
+    it 'should be threadsafe' do
+      threads = []
+      (1..10).each do |i|
+        threads << Thread.new do
+          expect(UCL.dump({ 'foo' => i })).to eq("foo = #{i};\n")
+        end
+      end
+
+      threads.map(&:join)
     end
   end
 
